1cc985fc0cb9d0f2c84517ec1e370bf0040faccd
Andrej Hafner <andrej.hafn3r@gmail.com>
2021-02-25 20:27:09 +0100

    aaa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from multiprocessing import Pool
 from tqdm import tqdm
 
@@ -33,12 +34,46 @@ def read_input(filename):
             paths.append(path_data[1:])
 
 
-    return duration, intersections, streets, paths, bonus_points
+    return duration, n_intersections, streets, paths, bonus_points
 
 
 
 
 if __name__ == '__main__':
-    duration, intersections, streets, paths, bonus_points = read_input("data/a.txt")
+
+    filename = "data/c.txt"
+
+    duration, n_intersections, streets, paths, bonus_points = read_input(filename)
+
+    # Get the frequency of the street visits
+    street_freq = defaultdict(int)
+    for car_path in paths:
+        for visited_street in car_path[:-1]:
+            street_freq[visited_street] += 1
+
+    intersection_interval = 10
+
+    intersections = {}
+
+    for i in range(n_intersections):
+        streets_in = [street[2] for street in streets if int(street[1]) == i]
+        intersections[i] = {street: street_freq[street] for street in streets_in if street_freq[street] > 0}
+
+
+    with open(f"out_{filename.replace('data/','')}", "w+") as f:
+        intersections = {key: intersections[key] for key in intersections.keys() if len(intersections[key]) > 0}
+        output = f"{len(intersections)}\n"
+        for key in intersections.keys():
+            output += (f"{key}\n")
+            output += (f"{len(intersections[key])}\n")
+            for street in intersections[key].keys():
+                output +=(f"{street} {intersections[key][street]}\n")
+
+
+        f.write(output.rstrip())
+
+
+
+
 
 
